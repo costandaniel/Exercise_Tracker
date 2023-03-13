58a90e79ec077ec8a16edeec2576c09edf059b66
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-03-14 01:57:59 +0200

    only one last challenge left

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGO_URI, () => {
   console.log("Db succesfuly");
 });
 mongoose.set("strictQuery", false);
-//db stuff
+//db stuff start
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -28,7 +28,7 @@ const exerciseSchema = mongoose.Schema({
   userId: mongoose.Types.ObjectId,
 });
 const Exercise = mongoose.model("Exercise", exerciseSchema);
-//db stuff
+//db stuff end
 const { urlencoded } = require("express");
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -36,8 +36,8 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
-//Get request to /api/users
-
+//Get request to /api/users START
+//
 app.get("/api/users", async (req, res, next) => {
   try {
     const users = await User.find({}, "-__v");
@@ -46,8 +46,11 @@ app.get("/api/users", async (req, res, next) => {
     return next(error);
   }
 });
+//
+//Get request to /api/users END
 
-//Post to /api/users
+//Post to /api/users START
+//
 app.post("/api/users", async (req, res, next) => {
   try {
     const username = req.body.username;
@@ -71,8 +74,12 @@ app.post("/api/users", async (req, res, next) => {
     return next(error);
   }
 });
+//
+//Post to /api/users END
 
-// Post to /api/users/:_id/exercices
+// Post to /api/users/:_id/exercices START
+//
+
 app.post("/api/users/:_id/exercises", async (req, res, next) => {
   try {
     const { description, duration, date, userId } = await Exercise.create({
@@ -93,6 +100,41 @@ app.post("/api/users/:_id/exercises", async (req, res, next) => {
   }
 });
 
+//
+// Post to /api/users/:_id/exercices END
+
+//GET request to /api/users/:_id/logs START
+//
+
+app.get("/api/users/:_id/logs", async (req, res, next) => {
+  try {
+    const userId = req.params._id;
+    const { username } = await User.findById(userId);
+    if (!username) {
+      res.json({ message: "No user with that id found" });
+    }
+
+    const exercices = await Exercise.find({ userId });
+    exercises = exercices.map((exercices) => {
+      return {
+        description: exercices.description,
+        duration: exercices.duration,
+        date: exercices.date.toDateString(),
+      };
+    });
+    res.json({
+      username: username,
+      count: exercices.length,
+      _id: userId,
+      log: exercises,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//
+//GET request to /api/users/:_id/logs END
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
